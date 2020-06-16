7f738622b662e0454c3cb1b86b12568da58e6fb5
philrussel21 <phil.antiporda21@gmail.com>
2020-06-17 00:55:16 +1000

    Implemented CREATE for blogs and comments

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,9 +8,17 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @new_blog = Blog.new()
   end
 
   def create
+    @new_blog = Blog.new(blog_params)
+
+    if @new_blog.save
+      redirect_to(root_path)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -21,4 +29,9 @@ class BlogsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def blog_params
+      params.require(:blog).permit(:title, :location, :country, :budget, :activity, :content)
+    end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,7 @@
 class Blog < ApplicationRecord
-    has_many :comments
+    has_many :comments, dependent: :destroy
+    validates_associated :comments
+    validates :title, presence: true, length: {maximum: 60}
+    validates :location, presence: true
+    validates :content, presence: true, length: {minimum: 50}
 end
