40f968e5de5cca817cec6994d73697a5b75b2206
philrussel21 <phil.antiporda21@gmail.com>
2020-06-20 00:35:50 +1000

    Added form styling on #new and #edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_132104) do
+ActiveRecord::Schema.define(version: 2020_06_19_105347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_06_15_132104) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "img"
   end
 
   create_table "comments", force: :cascade do |t|
